dd13fd4ea25c89ef42edfa4175ee796c4312a680
achlaq <achlaq.arif@muf.co.id>
2025-12-07 09:42:17 +0700

    feat(ui): add reusable rocket form component

--- components.d.ts
+++ components.d.ts
@@ -9,6 +9,7 @@ declare module 'vue' {
   export interface GlobalComponents {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RocketCard: typeof import('./src/components/RocketCard.vue')['default']
+    RocketForm: typeof import('./src/components/RocketForm.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
