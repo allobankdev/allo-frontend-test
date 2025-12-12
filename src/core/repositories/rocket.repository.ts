ad15ebe7cfe5148a5e969b328a83d7017c7717c9
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2025-12-12 14:32:30 +0700

    feat: add rocket repository

---
+++ src/core/repositories/rocket.repository.ts
@@ -0,0 +1,33 @@
+import type { RocketEntity } from "../entities/rocket.entity";
+
+export interface QueryOptions {
+  query?: Record<string, any>;
+  options?: {
+    select?: string[];
+    sort?: Record<string, number>;
+    limit?: number;
+    page?: number;
+    offset?: number;
+  };
+}
+
+export interface QueryResponse<T> {
+  docs: T[];
+  totalDocs: number;
+  offset: number;
+  limit: number;
+  totalPages: number;
+  page: number;
+  pagingCounter: number;
+  hasPrevPage: boolean;
+  hasNextPage: boolean;
+  prevPage: number;
+  nextPage: number;
+}
+
+export abstract class RocketRepository {
+  abstract fetchOne(rocketId: string): Promise<RocketEntity>;
+  abstract fetchAll(
+    queryOptions: QueryOptions
+  ): Promise<QueryResponse<RocketEntity>>;
+}
