f0623ec2680f8f6484554411cd2925d28979f604
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2025-12-13 09:15:45 +0700

    feat: add empty state component

---
+++ src/pages/rockets/components/EmptyState.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-col cols="12">
+    <v-alert type="info">
+      <v-alert-title>No Data</v-alert-title>
+      Click "Load Rockets" button to fetch SpaceX rockets data.
+    </v-alert>
+  </v-col>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
