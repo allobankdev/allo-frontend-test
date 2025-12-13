630138fa15afd3bce0adbee98f3793b000c030d4
leonaldopasaribu <leonaldopasaribu13@yahoo.com>
2025-12-13 09:13:53 +0700

    feat: add loading state component

---
+++ src/components/LoadingState.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-row>
+    <v-col cols="12" class="text-center">
+      <v-progress-circular
+        indeterminate
+        color="primary"
+        :size="size"
+      ></v-progress-circular>
+      <p class="mt-4">{{ message }}</p>
+    </v-col>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  message?: string;
+  size?: number | string;
+}
+
+withDefaults(defineProps<Props>(), {
+  message: "Loading...",
+  size: 64,
+});
+</script>
+
+<style scoped></style>
