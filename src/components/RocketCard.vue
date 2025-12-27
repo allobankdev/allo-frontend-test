b0c91b5b780209f3e34714f6cc624295a58f0865
ariokt <ariowta@gmail.com>
2025-12-27 13:11:56 +0700

    feat: List rocket page

---
+++ src/components/RocketCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { Rocket } from "@/api/getRockets/types";
+import { computed } from "vue";
+
+const { rocket } = defineProps<{
+  rocket: Rocket;
+}>();
+
+const shortDescription = computed(() => {
+  const desc = rocket.description;
+  return desc.length > 100 ? desc.slice(0, 100) + "..." : desc;
+});
+</script>
+
+<template>
+  <div
+    class="text-decoration-none"
+  >
+    <v-card class="mx-auto">
+      <v-card-item>
+        <v-img cover height="200" :src="rocket.flickr_images[0]" class="mb-2" />
+        <v-card-title>{{ rocket.name }}</v-card-title>
+      </v-card-item>
+      <v-card-text>
+        {{ shortDescription }}
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,48 @@
-<template>
-  <HelloWorld />
-</template>
+<script setup lang="ts">
+import { useRocketStore } from "@/stores/rocket";
+import { computed, onMounted, ref } from "vue";
 
-<script lang="ts" setup>
-  //
+const store = useRocketStore();
+const search = ref("");
+
+onMounted(async () => {
+  await store.fetchRockets();
+});
+
+const filteredRockets = computed(() => {
+  if (!search.value) return store.rockets;
+  const filterValue = search.value.toLowerCase();
+  return store.rockets.filter((rocket) =>
+    rocket.name.toLowerCase().includes(filterValue)
+  );
+});
 </script>
+
+<template>
+  <v-container class="py-8">
+    <div>
+      <div class="d-flex align-sm-center justify-space-between mb-8">
+        <h1>Rocket List</h1>
+      </div>
+      <FilterSearch v-model:filterKey="search" />
+      <div v-if="store.loading" class="d-flex justify-center align-center h-50">
+        <v-progress-circular indeterminate color="primary" size="64" />
+      </div>
+
+      <v-row>
+        <v-col
+          v-for="rocket in filteredRockets"
+          :key="rocket.id"
+          cols="12"
+          sm="6"
+          md="4"
+          lg="3"
+        >
+          <v-card-item class="pa-0">
+            <RocketCard :rocket="rocket" />
+          </v-card-item>
+        </v-col>
+      </v-row>
+    </div>
+  </v-container>
+</template>
