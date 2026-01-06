480adf08bf66ca8d1e0fd6b6a0f941854a03b42b
hasan <hasanalmunawar07@gmail.com>
2026-01-06 13:51:49 +0700

    feat: register rocket list page using file-based routing

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,52 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRocketStore } from '@/store/rocket'
+import RocketCard from "@/components/RocketCard.vue";
+const rocketStore = useRocketStore()
+
+onMounted(() => {
+  rocketStore.loadRockets()
+})
+</script>
+
 <template>
-  <HelloWorld />
+  <section>
+    <!-- Loading State -->
+    <div v-if="rocketStore.isLoading">
+      <p>Loading rockets...</p>
+    </div>
+
+    <!-- Error State -->
+    <div v-else-if="rocketStore.error">
+      <p>{{ rocketStore.error }}</p>
+      <button @click="rocketStore.loadRockets">
+        Retry
+      </button>
+    </div>
+
+    <!-- Success State -->
+    <div v-else>
+<!--      <ul>-->
+<!--        <li-->
+<!--          v-for="rocket in rocketStore.rockets"-->
+<!--          :key="rocket.id"-->
+<!--        >-->
+<!--          <h2>{{ rocket.id }}</h2>-->
+<!--          <h3>{{ rocket.name }}</h3>-->
+<!--          <p>{{ rocket.description }}</p>-->
+<!--        </li>-->
+<!--      </ul>-->
+      <RocketCard
+        v-for="rocket in rocketStore.rockets"
+        :key="rocket.id"
+        :rocket="rocket"
+      />
+
+    </div>
+  </section>
 </template>
 
-<script lang="ts" setup>
-  //
-</script>
+<style scoped>
+
+</style>
+
